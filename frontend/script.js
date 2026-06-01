c0f9f26c80ffd7280f983077c5f73f689d640755
tumainih <hoseatumaini12@gmail.com>
2026-06-01 21:18:00 +0300

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -24,3 +24,10 @@ async function getMessage() {
 
     }
 }
+function openSidebar() {
+    document.getElementById("sidebar").style.width = "250px";
+}
+
+function closeSidebar() {
+    document.getElementById("sidebar").style.width = "0";
+}
